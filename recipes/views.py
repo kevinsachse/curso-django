3c129dbbf4d6c62693202827ed3e57e0bd7207d5
Kevin Sachse <kevin.sachse@outlook.com.br>
2024-12-18 17:16:25 -0300

    1

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.shortcuts import render
 
 def home(request):
-    return render(request, 'recipes/home.html')
-
-def contato(request):
-    return HttpResponse('contato')
-
-def sobre(request):
-    return HttpResponse('sobre')
+    return render(request, 'recipes/pages/home.html')
