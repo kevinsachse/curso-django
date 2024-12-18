57ffa8895c932542fb1af2b4a4a407f89a28a778
Kevin Sachse <kevin.sachse@outlook.com.br>
2024-12-18 15:27:54 -0300

    commit

---
+++ recipes/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'recipes/home.html')
+
+def contato(request):
+    return HttpResponse('contato')
+
+def sobre(request):
+    return HttpResponse('sobre')
